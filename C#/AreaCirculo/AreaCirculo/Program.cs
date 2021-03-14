0d7762189846ec0b58504bdf51a4f3528d1b69a6
Augusto Souza <augusto_souza@edu.unifil.br>
2021-03-14 18:51:46 -0300

    Desafio em C#

---
+++ C#/AreaCirculo/AreaCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio;
+            double n = 3.14159;
+
+            raio = System.Console.Read();
+
+            area = Math.Pow(raio, 2.00) * n;
+
+            System.Console.WriteLine("A = " + area);
+
+        }
+    }
+}
