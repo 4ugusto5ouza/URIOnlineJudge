720561bc09f689c3abfd9a1040bc1fe0c3732739
Augusto Souza <augusto_souza@edu.unifil.br>
2021-03-14 17:52:35 -0300

    desafio 1002

---
+++ Java/AreaCirculo/src/AreaCirculo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AreaCirculo {
+
+/*    
+    A fórmula para calcular a área de uma circunferência é:area= π . raio2.
+    Considerando para este problema que π = 3.14159:
+    Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando
+    por π.
+    Entrada
+    A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+    Saída
+    Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error". */
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        double area, raio;
+        final double n = 3.14159;
+
+        raio = sc.nextDouble(); sc.nextLine();
+
+        area = Math.pow(raio, 2) * n;
+
+        System.out.printf("A = %.4f%n",(area));
+        sc.close();
+    }
+}
