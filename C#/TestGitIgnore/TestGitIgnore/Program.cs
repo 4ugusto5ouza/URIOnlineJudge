10cd09a218a3f215bbfc581813dbe7fa70887e4b
Augusto Souza <augusto_souza@edu.unifil.br>
2021-03-14 19:23:37 -0300

    Teste GitIgnore

---
+++ C#/TestGitIgnore/TestGitIgnore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGitIgnore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
